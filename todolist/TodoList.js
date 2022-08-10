3e428b121479bb0635e163123f66cb27934aed52
martucazpo <martucazpo@gmail.com>
2022-08-10 11:43:08 -0700

    set width and height from element

--- todolist/TodoList.js
+++ todolist/TodoList.js
@@ -1,18 +1,23 @@
-import TaskList from "./TaskList.js"
+import TaskList from "./TaskList.js";
 
 class TodoList extends HTMLElement {
-    constructor(){
-        super()
-        const shadow = this.attachShadow({mode:'open'})
-        const root = document.createElement("div")
-        root.setAttribute("class", "wrapper")
-        const link = document.createElement("link")
-        link.setAttribute("rel", "stylesheet")
-        link.setAttribute("href", "./todolist/styles.css")
-        shadow.append(link)
-        shadow.append(root)
-        new TaskList(root).render()
-    }
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    const root = document.createElement("div");
+    root.setAttribute("class", "wrapper");
+    const link = document.createElement("link");
+    link.setAttribute("rel", "stylesheet");
+    link.setAttribute("href", "./todolist/styles.css");
+    shadow.append(link);
+    shadow.append(root);
+    new TaskList(root).render();
+  }
+  connectedCallback() {
+    let wrapper = this.shadowRoot.querySelector(".wrapper")
+    wrapper.style.width = this.getAttribute("width");
+    wrapper.style.height = this.getAttribute("height");
+  }
 }
 
-export default TodoList
+export default TodoList;
